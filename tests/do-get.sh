e83e6f7f9ef4cda4a7ca1979a59e02db5f89e80c
Adam Ahmed <avahmed@ualberta.ca>
2023-02-04 22:48:43 -0700

    fix exit code

--- tests/do-get.sh
+++ tests/do-get.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 PYTHON=/usr/bin/python3.8
-e=0
+EXIT_CODE=0
 
 do_it () {
     url=$1
     $PYTHON httpclient.py GET $1 >/dev/null
-    e=$?; if [ $e -ne 0 ]; then echo "ERR Failed GET $1"; fi
+    e=$?; if [ $e -ne 0 ]; then echo "ERR Failed GET $1"; EXIT_CODE=$e; fi
 }
 
 do_it 'http://example.com'
@@ -14,4 +14,4 @@ do_it 'http://www.cs.ualberta.ca/'
 do_it 'http://softwareprocess.es/static/SoftwareProcess.es.html'
 do_it 'http://c2.com/cgi/wiki?CommonLispHyperSpec'
 do_it 'http://slashdot.org'
-if [ $e -eq 0 ]; then echo "OK All pass"; fi
+if [ $EXIT_CODE -eq 0 ]; then echo "OK All pass"; fi
